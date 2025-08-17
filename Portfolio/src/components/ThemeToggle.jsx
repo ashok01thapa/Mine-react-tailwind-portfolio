cfeacaaeeedb2beffddfdfa521bcb9e3f0b6cb4b
Ashok Thapa <153058399+ashok01thapa@users.noreply.github.com>
2025-08-17 13:45:12 +0545

    Update ThemeToggle.jsx

--- Portfolio/src/components/ThemeToggle.jsx
+++ Portfolio/src/components/ThemeToggle.jsx
@@ -35,7 +35,7 @@ const ThemeToggle = () => {
     }
   return (
    <button onClick={toggleTheme}
-    className= {cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300 ",
+    className= {cn("fixed top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300 ",
         "focus:outline-hidden"
     )}
     >
